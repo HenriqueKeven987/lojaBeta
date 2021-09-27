0501646a43a3a47e6b8ebc17b0ca38b3f9cda087
HenriqueKeven987 <henrique.keven@seduc.ce.gov.br>
2021-09-27 12:05:22 -0300

    git world

---
+++ src/application/program.java
@@ -0,0 +1,52 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.products;
+
+public class program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner entrada = new Scanner(System.in);
+		
+		/*System.out.println(product.nome);
+		System.out.println(product.preco);
+		System.out.println(product.quantidade);*/
+		
+		
+		System.out.println("Entre com os dados do produto");
+		System.out.print("Nome: ");
+		String nome = entrada.nextLine();
+		/*System.out.print("Quantidade: ");
+		int quantidade = entrada.nextInt();*/
+		System.out.print("Preço: ");
+		double preco = entrada.nextDouble();		
+		
+		products product = new products();
+		product.setNomePreco(nome, preco);
+		
+		
+		System.out.println("Dados do produto: "+ product);
+		
+		System.out.print("entre com o numero de produtos a ser adicionado ao Estoque: ");
+		product.addStock(entrada.nextInt());
+		System.out.println("Dados do produto: "+ product);
+		
+		/*product.setNome("alura");
+		nome = product.getNome();*/
+		System.out.println("Nome atualizado: "+ nome);
+		
+		System.out.print("quantos produtos vao ser retirados do estoque: ");
+		product.rmStock(entrada.nextInt());
+		System.out.println("Dados do produto: "+ product);
+		
+		
+		entrada.close();
+		
+
+	}
+
+}
---
+++ src/entities/products.java
@@ -0,0 +1,55 @@
+package entities;
+
+public class products {
+	
+	private String nome;
+	private double preco;
+	private int quantidade;
+	
+	public products () {
+		
+	}
+	
+	public products (String nome, double preco, int quantidade) {
+		this.nome = nome;
+		this.preco = preco;
+		this.quantidade = quantidade;
+	}
+	
+	public products (String nome, double preco) {
+		this.nome = nome;
+		this.preco = preco;
+		quantidade = 0;
+	}
+	
+	public void setNomePreco(String nome, double preco) {
+		this.nome = nome;
+		this.preco = preco;		
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public double totalValue() {		
+		return preco * quantidade;
+	}
+	
+	public void addStock(int quantidade) {
+		this.quantidade += quantidade;		
+	}
+	
+	public void rmStock(int quantidade) {
+		this.quantidade -= quantidade;
+	}
+	
+	public String toString() {
+		
+		return nome + ", $" + String.format("%.2f",preco) + ", Qtd " + quantidade + " Total: $" + String.format("%.2f",totalValue());
+	}
+	
+}
